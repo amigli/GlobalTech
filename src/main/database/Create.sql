3a432052ef580c57fdaa740047fe631c7be1aa76
migli <miglinoannalaura@gmail.com>
2022-04-29 20:21:23 +0200

    Creazione tabelle

--- src/main/database/Create.sql
+++ src/main/database/Create.sql
@@ -26,13 +26,52 @@ CREATE TABLE Telefonia (
     fotocamera_anteriore float,
     cpu_nome varchar(20),
     cpu_hertz float not null,
-
-
-
-
-
 )
 
+CREATE TABLE Utente (
+    ID int auto_increment primary key,
+    nome_utente char (30) not null,
+    password varchar (20) not null,
+    data_nascita date not null,
+    nome char (30) not null,
+    cognome char (30) not null,
+    indirizzo char (50) not null,
+    num_acquisti int not null,
+    admin_yn boolean not null
+);
 
+CREATE TABLE Carrello (
+    IDutente int references Utente (ID) primary key,
+    totale double not null
+);
 
+CREATE TABLE Ordine (
+    ID int auto_increment primary key,
+    prezzo_totale float not null,
+    data_o date not null,
+    spese_spedizione float not null,
+    modalita_pagamento char (30) not null,
+    stato char (30) not null,
+    IDutente int references Utente (ID)
+);
 
+CREATE TABLE Contenere (
+    IDprodotto int references Prodotto(id),
+    IDordine int references Ordine(ID),
+    quantita int not null,
+    prezzo_acquisto double not null,
+    primary key (IDprodotto, IDordine)
+);
+
+CREATE TABLE Offerta (
+    ID int auto_increment primary key,
+    percentuale double not null,
+    data_inizio date not null,
+    data_fine date not null
+);
+
+CREATE TABLE Applicare(
+    IDprodotto int references Prodotto(id),
+    IDofferta int references Offerta(ID),
+    primary key(IDprodotto, IDofferta)
+);
