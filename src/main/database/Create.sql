4407e481505b4cfb554910ec43d0b80a88fc0808
frankzamma <frank581.fgz@gmail.com>
2022-04-29 20:07:08 +0200

    Aggiunta tabelle

--- src/main/database/Create.sql
+++ src/main/database/Create.sql
@@ -15,5 +15,24 @@ CREATE TABLE Foto (
     primary key(numero, id_prodotto),
 );
 
+CREATE TABLE Telefonia (
+    id_prodotto int not null references  Prodotto(id) primary key ,
+    batteria_tipo varchar(20) not null,
+    batteria_capacita float not null,
+    ram_tipo varchar(20) not null,
+    ram_quantita float(20) not null,
+    so varchar(20) not null,
+    fotocamera_posteriore float,
+    fotocamera_anteriore float,
+    cpu_nome varchar(20),
+    cpu_hertz float not null,
+
+
+
+
+
+)
+
+
 
 
