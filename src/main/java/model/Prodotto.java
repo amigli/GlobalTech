dd38ac9104c7e685e690bd1d266cbc048ba0fe35
frankzamma <frank581.fgz@gmail.com>
2022-05-02 18:23:49 +0200

    Aggiunta Prodotto.java

--- src/main/java/model/Prodotto.java
+++ src/main/java/model/Prodotto.java
@@ -1,7 +1,77 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Prodotto{
-    int ID, quantita;
+    int id, quantita;
+    String nome, marca, colore, descrizione;
+    ArrayList<String> immagini;
+    double prezzoListino;
 
+    public Prodotto() {
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantita() {
+        return quantita;
+    }
+
+    public void setQuantita(int quantita) {
+        this.quantita = quantita;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getColore() {
+        return colore;
+    }
+
+    public void setColore(String colore) {
+        this.colore = colore;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public ArrayList<String> getImmagini() {
+        return immagini;
+    }
+
+    public void setImmagini(ArrayList<String> immagini) {
+        this.immagini = immagini;
+    }
+
+    public double getPrezzoListino() {
+        return prezzoListino;
+    }
+
+    public void setPrezzoListino(double prezzoListino) {
+        this.prezzoListino = prezzoListino;
+    }
 }
