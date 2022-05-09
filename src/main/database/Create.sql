bbd0e8d5490fcbb1497c97f6e7cbbb6fa0bbb322
migli <miglinoannalaura@gmail.com>
2022-05-09 21:24:13 +0200

    Aggiornamento file SQL

--- src/main/database/Create.sql
+++ src/main/database/Create.sql
@@ -11,19 +11,26 @@ CREATE TABLE Prodotto(
      colore varchar(50) not null,
      prezzoListino float not null,
      descrizione char(50),
-     batteria_tipo varchar(20),
-     batteria_capacita float,
-     ram_tipo varchar(20),
-     ram_quantita float(20),
+     batteriaTipo varchar(20),
+     batteriaCapacita float,
+     ramTipo varchar(20),
+     ramQuantita float(20),
      so varchar(20),
-     fotocamera_posteriore float,
-     fotocamera_anteriore float,
-     cpu_nome varchar(20),
-     cpu_hertz float,
+     fotocameraPosteriore float,
+     fotocameraAnteriore float,
+     cpuNome varchar(20),
+     cpuHertz float,
      gpu varchar(20),
      schermo varchar(30),
      autonomia int
+);
 
+CREATE TABLE Foto (
+  numero int not null,
+  id_prodotto int not null references  Prodotto(id),
+  estensione  varchar(4) not null,
+
+  primary key(numero, id_prodotto)
 );
 
 CREATE TABLE Categoria(
@@ -36,67 +43,82 @@ CREATE TABLE Appartenere(
     id_categoria int references Categoria(id),
     id_prodotto int references Prodotto(id)
 );
-CREATE TABLE Foto (
-    numero int not null,
-    id_prodotto int not null references  Prodotto(id),
-    estensione  varchar(4) not null,
 
-    primary key(numero, id_prodotto)
-);
 
 CREATE TABLE Utente (
     id int auto_increment primary key,
-    nome_utente char (30) not null,
+    nomeUtente char (30) not null,
     password varchar (20) not null,
-    data_nascita date not null,
+    dataNascita date not null,
     nome char (30) not null,
     cognome char (30) not null,
-    indirizzo char (50) not null,
-    num_acquisti int not null,
     admin boolean not null
 );
 
 CREATE TABLE Carrello (
-    id_utente int primary key references Utente (id) ,
+    idUtente int primary key references Utente (id) ,
     totale double not null
 );
 
 CREATE TABLE Ordine (
     id int auto_increment primary key,
-    prezzo_totale float not null,
-    data_o date not null,
-    spese_spedizione float not null,
-    modalita_pagamento char (30) not null,
+    prezzoTotale float not null,
+    tracking char (20),
+    dataO date not null,
+    speseSpedizione float not null,
+    modalitaPagamento char (30) not null,
     stato char (30) not null,
-    id_utente int references Utente (id)
+    idUtente int references Utente (id)
 );
 
 CREATE TABLE Contenere (
-    id_prodotto int references Prodotto(id),
-    id_ordine int references Ordine(ID),
+    idProdotto int references Prodotto(id),
+    idOrdine int references Ordine(ID),
     quantita int not null,
-    prezzo_acquisto double not null,
-    primary key (id_prodotto, id_ordine)
+    prezzoAcquisto double not null,
+    primary key (idProdotto, idOrdine)
 );
 
 CREATE TABLE Offerta (
     id int auto_increment primary key,
     percentuale double not null,
-    data_inizio date not null,
-    data_fine date not null
+    dataInizio date not null,
+    dataFine date not null
 );
 
 CREATE TABLE Applicare(
-    id_prodotto int references Prodotto(id),
-    id_offerta int references Offerta(id),
-    primary key(id_prodotto, id_offerta)
+    idProdotto int references Prodotto(id),
+    idOfferta int references Offerta(id),
+    primary key(idProdotto, idOfferta)
 );
 
 
 CREATE TABLE Comporre(
-    id_utente int not null references  Utente(id) ,
-    id_prodotto int not null references prodotto(id),
+    idUtente int not null references  Utente(id) ,
+    idProdotto int not null references prodotto(id),
     quantita int not null,
+    prezzo double not null,
 
-    primary key(id_utente, id_prodotto)
+    primary key(idUtente, idProdotto)
+);
+
+CREATE TABLE Magazzino (
+    id int not null primary key,
+    citta char (30)
+);
+
+CREATE TABLE Riserva(
+    idProdotto int not null references Prodotto(id),
+    idMagazzino int not null references Magazzino(id),
+    quantita int not null
+);
+
+CREATE TABLE Cliente(
+    idUtente int not null primary key references Utente(id),
+    viaC char (30) not null,
+    civico int not null,
+    citta char (30) not null,
+    cap int not null,
+    numAcquisti int not null,
+    numeroTelefono char(10) not null
 );
