6361f5bb9b72cfcc0531fb00786b16ed4ff794d5
frankzamma <frank581.fgz@gmail.com>
2022-05-20 23:18:21 +0200

    Aggiunta Item.java

---
+++ src/main/java/model/Item.java
@@ -0,0 +1,34 @@
+package model;
+
+public class Item {
+    private Prodotto prodotto;
+    private int quantita;
+    private float prezzo;
+
+    public Item() {
+    }
+
+    public Prodotto getProdotto() {
+        return prodotto;
+    }
+
+    public void setProdotto(Prodotto prodotto) {
+        this.prodotto = prodotto;
+    }
+
+    public int getQuantita() {
+        return quantita;
+    }
+
+    public void setQuantita(int quantita) {
+        this.quantita = quantita;
+    }
+
+    public float getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(float prezzo) {
+        this.prezzo = prezzo;
+    }
+}
