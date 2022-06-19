f9edded272c90410a0b54f7e62ed21683d502228
migli <miglinoannalaura@gmail.com>
2022-06-19 19:58:34 +0200

    Fatta servlet di accesso

---
+++ src/main/java/Controller/AccessoUtente.java
@@ -0,0 +1,42 @@
+package controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Utente;
+import model.UtenteDAO;
+
+import java.io.IOException;
+
+@WebServlet(name = "AccessoUtenteServlet", value = "/accesso-utente")
+public class AccessoUtente extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        UtenteDAO service = new UtenteDAO();
+        String address;
+
+        /*
+        * I controlli non li inserisco perché si suppone che, essendo fatti già in fase di registrazione,
+        * sia email e password siano coerenti con quelli che sono i formati richiesti.
+        * */
+
+        Utente u=service.login(email, password);
+        if (u!=null){
+            if (u.isAdmin())
+                address="#"; //pagina che gestisce l'admin (ancora da fare)
+            else
+                address="#"; //home del sito
+        }
+        else{
+            //anche qui inserire un messaggio nella pagina di login che indica che qualcosa è andato storto
+            address = "/loginPage.jsp";
+        }
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher(address);
+        dispatcher.forward(request, response);
+    }
+}
