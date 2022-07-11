6f006fee4f290996fbfdca202aad17a5f0f1fa1a
migli <miglinoannalaura@gmail.com>
2022-07-11 15:24:35 +0200

    Aggiornato Date con LocalDate e aggiunta di metodi per tracking

--- src/main/java/model/OrdineDAO.java
+++ src/main/java/model/OrdineDAO.java
@@ -1,11 +1,7 @@
 package model;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
-import java.util.GregorianCalendar;
 import java.util.List;
 
 public class OrdineDAO {
@@ -17,15 +13,15 @@ public class OrdineDAO {
             while (rs.next()){
                 int id= rs.getInt(1);
                 double prezzoTotale=rs.getDouble(2);
-                GregorianCalendar data= (GregorianCalendar) rs.getObject(3);
-                double speseSpedizione=rs.getDouble(4);
-                String modalitaPagamento=rs.getString(5);
-                String stato=rs.getString(6);
+                Date data = rs.getDate(4);
+                double speseSpedizione=rs.getDouble(5);
+                String modalitaPagamento=rs.getString(6);
+                String stato=rs.getString(7);
 
                 Ordine o=new Ordine();
                 o.setId(id);
                 o.setPrezzoTotale(prezzoTotale);
-                o.setData(data);
+                o.setData(data.toLocalDate());
                 o.setSpeseSpedizione(speseSpedizione);
                 o.setModalitaPagamento(modalitaPagamento);
                 o.setStato(stato);
@@ -47,14 +43,14 @@ public class OrdineDAO {
             if (rs.next()){
                 int ID= rs.getInt(1);
                 double prezzoTotale=rs.getDouble(2);
-                GregorianCalendar data= (GregorianCalendar) rs.getObject(3);
-                double speseSpedizione=rs.getDouble(4);
-                String modalitaPagamento=rs.getString(5);
-                String stato=rs.getString(6);
+                Date data= rs.getDate(4);
+                double speseSpedizione=rs.getDouble(5);
+                String modalitaPagamento=rs.getString(6);
+                String stato=rs.getString(7);
 
                 o.setId(ID);
                 o.setPrezzoTotale(prezzoTotale);
-                o.setData(data);
+                o.setData(data.toLocalDate());
                 o.setSpeseSpedizione(speseSpedizione);
                 o.setModalitaPagamento(modalitaPagamento);
                 o.setStato(stato);
@@ -74,15 +70,15 @@ public class OrdineDAO {
             while (rs.next()){
                 int ID= rs.getInt(1);
                 double prezzoTotale=rs.getDouble(2);
-                GregorianCalendar data= (GregorianCalendar) rs.getObject(3);
-                double speseSpedizione=rs.getDouble(4);
-                String modalitaPagamento=rs.getString(5);
-                String stato=rs.getString(6);
+                Date data= rs.getDate(4);
+                double speseSpedizione=rs.getDouble(5);
+                String modalitaPagamento=rs.getString(6);
+                String stato=rs.getString(7);
 
                 Ordine o = new Ordine();
                 o.setId(ID);
                 o.setPrezzoTotale(prezzoTotale);
-                o.setData(data);
+                o.setData(data.toLocalDate());
                 o.setSpeseSpedizione(speseSpedizione);
                 o.setModalitaPagamento(modalitaPagamento);
                 o.setStato(stato);
@@ -94,5 +90,33 @@ public class OrdineDAO {
             throw new RuntimeException(e);
         }
         return l;
+    }
+
+    public void setStatusById(String status, int id){
+        try (Connection con = ConPool.getConnection()) {
+            PreparedStatement stmt = con.prepareStatement("UPDATE ordine SET stato = ? WHERE id = ?");
+
+            stmt.setString(1, status);
+            stmt.setInt(2, id);
+
+            stmt.executeUpdate();
+            con.close();
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    public void setTrackingById(String track, int id){
+        try (Connection con = ConPool.getConnection()) {
+            PreparedStatement stmt = con.prepareStatement("UPDATE ordine SET tracking = ? WHERE id = ?");
+
+            stmt.setString(1, track);
+            stmt.setInt(2, id);
+
+            stmt.executeUpdate();
+            con.close();
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
     }
 }
